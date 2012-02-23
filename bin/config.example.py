2be5fa7593ec63b03b5b92823072627a3844a7c9
Andy Mastbaum <amastbaum@gmail.com>
2012-02-22 22:58:02 -0500

    Update bin/config.example.py

--- bin/config.example.py
+++ bin/config.example.py
@@ -3,6 +3,7 @@ from repoman.handlers import *
 git_url = 'git@github.com:OWNER/REPO'
 
 pytunia_submitter_settings = {
+    'db_url': 'http://HOST:5984/DBNAME',
     'git_url': git_url,
     'test_path': 'test/full',
     'changeset_base_url': 'https://github.com/OWNER/REPO/commit/',
@@ -12,10 +13,10 @@ pytunia_submitter_settings = {
 
 handlers = {
     'REPO': [
-        Repeater('http://localhost:8452', git_url),   # rebuild docs
-        Repeater('http://localhost:8453', git_url),   # mirror
-        Emailer(),                                    # email a list
-        PytuniaSubmitter(*pytunia_submitter_settings) # submit to build tester
+        Repeater('http://localhost:8452', git_url),      # rebuild docs
+        Repeater('http://localhost:8453', git_url),      # mirror
+        Emailer(['USER1@SITE1.COM', 'USER2@SITE2.COM']), #email a list
+        PytuniaSubmitter(*pytunia_submitter_settings)    # submit to build tester
     ]
 }
 
